c3a6058a8b3b931b7a4b9afbcfa3dbbeb3e7f037
Oussama Benloughmari <ben-oussama@hotmail.fr>
2020-01-03 06:44:33 +0100

    ajout de database

--- app/src/main/java/com/example/fenkayn/Registration.java
+++ app/src/main/java/com/example/fenkayn/Registration.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Registration extends AppCompatActivity {
 
@@ -15,9 +16,14 @@ public class Registration extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        DatabaseHelper mydb = new DatabaseHelper(this);
 
-
-
+        boolean isInserted = mydb.addUser("oussama@gmail.com","123","mehdiC","14/1/120","male");
+        if (isInserted = true){
+            Toast.makeText(Registration.this,"Data Inserted",Toast.LENGTH_LONG).show();
+        }else{
+            Toast.makeText(Registration.this,"Data Not Inserted",Toast.LENGTH_LONG).show();
+        }
 
         // login clicked
         login = findViewById(R.id.login);
